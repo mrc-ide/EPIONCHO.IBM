fca38d53241166e7dc93d84b9532cff5a288c108
adiramani <adiramani84@gmail.com>
2026-05-11 10:42:30 +0100

    Updating processing file

--- process_multiple_runs.R
+++ process_multiple_runs.R
@@ -1,4 +1,4 @@
-process_multiple_runs <- function (files='', ov16_indiv = FALSE, morbidity_runs = FALSE, verbose=TRUE, ov16_indiv_locations=c(1, 2, 3, 4, 5)) {
+process_multiple_runs <- function (files='', ov16_indiv = FALSE, morbidity_runs = FALSE, verbose=TRUE, ov16_indiv_locations=c(1), ov16_indiv_location_names=c("baseline"), min_iter=0, max_iter=100000) {
   allOutputs <- data.frame()
   fileToUse <- files
 
@@ -27,6 +27,15 @@ process_multiple_runs <- function (files='', ov16_indiv = FALSE, morbidity_runs 
         message(paste("Error occurred while reading:", fileToUse))
       }
     )
+
+    # Assume that the iter number is always the last value before '.rds'
+    # I.e output_...._17.rds, 17 is the iter number
+    iter <- strsplit(file, "_")[[1]]
+    iter <- as.numeric(strsplit(all_vals[length(all_vals)], "\\.")[[1]][1])
+    print(paste0("i: ", i, " Iter: ", iter))
+    if (iter > max_iter | iter < min_iter) {
+      next
+    }
     
     print(file)
     selector <- which(tmpRDSData$years %% 1 == 0)
@@ -38,14 +47,21 @@ process_multiple_runs <- function (files='', ov16_indiv = FALSE, morbidity_runs 
     num_individuals <- length(age)
     ov16_indiv_df_cols <- ncol(ov16_indiv_matrix) + 3
     if(i == 1) {
-      mf_prev_df <- matrix(ncol=ncol(tmpRDSData$all_mf_prevalence_age_grouped)+4, nrow=total_files*num_vals)
-      colnames(mf_prev_df) <- c(colnames(tmpRDSData$all_mf_prevalence_age_grouped), "Ke", "ABR", "year", 'run_num')
+      mf_prev_df <- matrix(ncol=(ncol(tmpRDSData$all_mf_prevalence_age_grouped)*2)+4, nrow=total_files*num_vals)
+      colnames(mf_prev_df) <- c(
+        colnames(tmpRDSData$all_mf_prevalence_age_grouped), 
+        # paste0("30-", colnames(tmpRDSData$all_mf_prevalence_age_grouped)), - TODO: need to update model
+        "Ke", "ABR", "year", 'run_num'
+      )
 
-      atp_df <- matrix(ncol=10, nrow=total_files*num_vals)
-      colnames(atp_df) <- c("l3_per_blackfly", "recorded_abr", "atp", "Ke", "ABR", "h", "m", "beta", "year", 'run_num')
+      atp_df <- matrix(ncol=11, nrow=total_files*num_vals)
+      colnames(atp_df) <- c("l3_per_blackfly", "l3_prevalence_per_blackfly", "recorded_abr", "atp", "Ke", "ABR", "h", "m", "beta", "year", 'run_num')
 
-      mf_intensity_df <- matrix(ncol=ncol(tmpRDSData$all_mf_intensity_age_grouped)+4, nrow=total_files*num_vals)
-      colnames(mf_intensity_df) <- c(colnames(tmpRDSData$all_mf_intensity_age_grouped), "Ke", "ABR", "year", 'run_num')
+      mf_intensity_df <- matrix(ncol=(ncol(tmpRDSData$all_mf_intensity_age_grouped)*2)+4, nrow=total_files*num_vals)
+      colnames(mf_intensity_df) <- c(
+        colnames(tmpRDSData$all_mf_intensity_age_grouped), 
+        # paste0("30-", colnames(tmpRDSData$all_mf_intensity_age_grouped)), - TODO: need to update model
+        "Ke", "ABR", "year", 'run_num')
       
       if (morbidity_runs) {
         morbidity_df <- matrix(ncol=ncol(tmpRDSData$all_morbidity_prevalence_outputs)+4, nrow=total_files*num_vals)
@@ -63,18 +79,17 @@ process_multiple_runs <- function (files='', ov16_indiv = FALSE, morbidity_runs 
       
       if (ov16_indiv) {
         ov16_indiv_df <- data.frame(matrix(ncol=ov16_indiv_df_cols, nrow=num_individuals*total_files))
-        ov16_indiv_df_col_names <- c()
-        ov16_loc_names <- c("pre_mda", "post_vc_1", "post_vc_2", "start_biannual_mda", "survey_time")
-        ov16_indiv_df_cols <- c("age", "sex", "mf_status", "mf_intens_raw", "ov16_status_no_seroreversion", "ov16_status_instant_serorevert", "ov16_status_finite_seroreversion")
+        ov16_indiv_df_final_col_names <- c()
+        ov16_indiv_df_names <- c("age", "sex", "mf_status", "mf_intens_raw", "ov16_status", "serorevert_fast")
         ov16_indiv_index <- 1
         for (loc in ov16_indiv_locations) {
-          ov16_indiv_df_col_names <- c(
-            ov16_indiv_df_col_names,
-            paste(ov16_loc_names[loc], ov16_indiv_df_cols, sep="_")
+          ov16_indiv_df_final_col_names <- c(
+            ov16_indiv_df_final_col_names,
+            paste(ov16_indiv_location_names[loc], ov16_indiv_df_names, sep="_")
           )
         }
-        ov16_indiv_df_col_names <- c(ov16_indiv_df_col_names, "ABR", "Ke", "run_num")
-        colnames(ov16_indiv_df) <- ov16_indiv_df_col_names
+        ov16_indiv_df_final_col_names <- c(ov16_indiv_df_final_col_names, "ABR", "Ke", "run_num")
+        colnames(ov16_indiv_df) <- ov16_indiv_df_final_col_names
       }
     }
     
@@ -83,28 +98,28 @@ process_multiple_runs <- function (files='', ov16_indiv = FALSE, morbidity_runs 
       kE <- tmpRDSData$Ke
     }
 
-    iter <- as.numeric(strsplit(strsplit(file, "_")[[1]][5], "\\.")[[1]][1])
-    print(paste0("i: ", i, " Iter: ", iter))
-
-    atp_df[(1+(num_vals*(i-1))):(i*num_vals),(ncol(atp_df)-9)] <- tmpRDSData$L3[selector]
+    atp_df[(1+(num_vals*(i-1))):(i*num_vals),(ncol(atp_df)-10)] <- tmpRDSData$L3[selector]
+    atp_df[(1+(num_vals*(i-1))):(i*num_vals),(ncol(atp_df)-9)] <- tmpRDSData$blackfly_l3_prevalence[selector]
     atp_df[(1+(num_vals*(i-1))):(i*num_vals),(ncol(atp_df)-8)] <- tmpRDSData$ABR_recorded[selector]
     atp_df[(1+(num_vals*(i-1))):(i*num_vals),(ncol(atp_df)-7)] <- tmpRDSData$L3[selector] * tmpRDSData$ABR_recorded[selector]
     atp_df[(1+(num_vals*(i-1))):(i*num_vals),(ncol(atp_df)-6)] <- rep(kE, length(selector))
     atp_df[(1+(num_vals*(i-1))):(i*num_vals),(ncol(atp_df)-5)] <- tmpRDSData$ABR
-    atp_df[(1+(num_vals*(i-1))):(i*num_vals),(ncol(atp_df)-4)] <- tmpRDSData$h_recorded[selector]
-    atp_df[(1+(num_vals*(i-1))):(i*num_vals),(ncol(atp_df)-3)] <- tmpRDSData$m_recorded[selector]
-    atp_df[(1+(num_vals*(i-1))):(i*num_vals),(ncol(atp_df)-2)] <- tmpRDSData$beta_recorded[selector]
+    atp_df[(1+(num_vals*(i-1))):(i*num_vals),(ncol(atp_df)-4)] <- NaN# tmpRDSData$h_recorded[selector] - TODO: need to update model
+    atp_df[(1+(num_vals*(i-1))):(i*num_vals),(ncol(atp_df)-3)] <- NaN# tmpRDSData$m_recorded[selector] - TODO: need to update model
+    atp_df[(1+(num_vals*(i-1))):(i*num_vals),(ncol(atp_df)-2)] <- NaN# tmpRDSData$beta_recorded[selector] - TODO: need to update model
     atp_df[(1+(num_vals*(i-1))):(i*num_vals),(ncol(atp_df)-1)] <- tmpRDSData$years[selector]
     atp_df[(1+(num_vals*(i-1))):(i*num_vals),(ncol(atp_df))] <- rep(iter, length(selector))
 
 
-    mf_prev_df[(1+(num_vals*(i-1))):(i*num_vals),1:(ncol(mf_prev_df)-4)] <- tmpRDSData$all_mf_prevalence_age_grouped[selector,]
+    mf_prev_df[(1+(num_vals*(i-1))):(i*num_vals),1:(ncol(tmpRDSData$all_mf_prevalence_age_grouped))] <- tmpRDSData$all_mf_prevalence_age_grouped[selector,]
+    # mf_prev_df[(1+(num_vals*(i-1))):(i*num_vals),(ncol(tmpRDSData$all_mf_prevalence_age_grouped)+1):(ncol(mf_prev_df)-4)] <- tmpRDSData$all_mf_prevalence_30_age_grouped[selector,]
     mf_prev_df[(1+(num_vals*(i-1))):(i*num_vals),ncol(mf_prev_df)-3] <- rep(kE, length(selector))
     mf_prev_df[(1+(num_vals*(i-1))):(i*num_vals),ncol(mf_prev_df)-2] <- tmpRDSData$ABR_recorded[selector]
     mf_prev_df[(1+(num_vals*(i-1))):(i*num_vals),ncol(mf_prev_df)-1] <- tmpRDSData$years[selector]
     mf_prev_df[(1+(num_vals*(i-1))):(i*num_vals),ncol(mf_prev_df)] <- rep(iter, length(selector))
     
-    mf_intensity_df[(1+(num_vals*(i-1))):(i*num_vals),1:(ncol(mf_intensity_df)-4)] <- tmpRDSData$all_mf_intensity_age_grouped[selector,]
+    mf_intensity_df[(1+(num_vals*(i-1))):(i*num_vals),1:(ncol(tmpRDSData$all_mf_intensity_age_grouped))] <- tmpRDSData$all_mf_intensity_age_grouped[selector,]
+    # mf_intensity_df[(1+(num_vals*(i-1))):(i*num_vals),(ncol(tmpRDSData$all_mf_intensity_age_grouped)+1):(ncol(mf_intensity_df)-4)] <- tmpRDSData$all_mf_intensity_30_age_grouped[selector,]
     mf_intensity_df[(1+(num_vals*(i-1))):(i*num_vals),ncol(mf_intensity_df)-3] <- rep(kE, length(selector))
     mf_intensity_df[(1+(num_vals*(i-1))):(i*num_vals),ncol(mf_intensity_df)-2] <- tmpRDSData$ABR_recorded[selector]
     mf_intensity_df[(1+(num_vals*(i-1))):(i*num_vals),ncol(mf_intensity_df)-1] <- tmpRDSData$years[selector]
@@ -141,17 +156,7 @@ process_multiple_runs <- function (files='', ov16_indiv = FALSE, morbidity_runs 
     if (ov16_indiv) {
       start_index <- 1+num_individuals*(i-1)
       end_index <- num_individuals*i
-      # list_of_ov16_indiv_outputs <- list()
-      # loc_index <- 0
-      # for (loc in ov16_indiv_locations) {
-      #   list_of_ov16_indiv_outputs[[(7*loc_index)+1]] <- ov16_indiv_matrix
-      #   list_of_ov16_indiv_outputs[[(7*loc_index)+2]] <- ifelse(ov16_indiv_matrix[,(loc*7)-5] == 1, "Male", "Female")
-      #   list_of_ov16_indiv_outputs[[(7*loc_index)+3]] <- ov16_indiv_matrix[,(loc*7)-1]
-      #   list_of_ov16_indiv_outputs[[(7*loc_index)+4]] <- ov16_indiv_matrix[,(loc*7)]
-      #   list_of_ov16_indiv_outputs[[(7*loc_index)+5]] <- ov16_indiv_matrix[,(loc*7)-2]
-      #   loc_index + 1
-      # }
-      ov16_indiv_df[start_index:end_index,1:(ov16_indiv_df_cols-3)] <- ov16_indiv_matrix#list_of_ov16_indiv_outputs
+      ov16_indiv_df[start_index:end_index,1:(ov16_indiv_df_cols-3)] <- ov16_indiv_matrix
       ov16_indiv_df[start_index:end_index,ov16_indiv_df_cols-2] <- tmpRDSData$ABR
       ov16_indiv_df[start_index:end_index,ov16_indiv_df_cols-1] <- kE
       ov16_indiv_df[start_index:end_index,ov16_indiv_df_cols] <- iter
