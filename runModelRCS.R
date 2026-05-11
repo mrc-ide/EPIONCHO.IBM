141618058c65200e9116b506e86ddc2cf9763e35
adiramani <adiramani84@gmail.com>
2026-05-11 17:24:18 +0100

    Updating RCS running files after testing

--- runModelRCS.R
+++ runModelRCS.R
@@ -3,18 +3,19 @@ output_folder = Sys.getenv("OUTPUT_FOLDER")
 iter <- as.numeric(Sys.getenv("PBS_ARRAY_INDEX"))
 set.seed(iter + (iter*3758))
 
-num_iters_per_paramset = 200
+iters_per_paramset = 200
+
 DT_in <- 1/366
 
-kEs = c(rep(0.2, num_iters/2), rep(0.3, num_iters/2))
+abrs_to_test = c(50, 70, 90, 100, 130, 160, 200, 300, 400, 500, 600, 800, 1000, 1250, 1500, 2000, 3000, 4000, 5000, 6000, 10000, 20000, 50000)
+
+
+
+kEs = rep(c(rep(0.2, iters_per_paramset), rep(0.3, iters_per_paramset), rep(0.4, iters_per_paramset)), each=length(abrs_to_test))
 kE <- kEs[iter]
 
-
-if (kE == 0.2) {
-  ABR_in <- 73
-} else {
-  ABR_in <- 176
-}
+# 200x50, 200x70, ... 200x50000, then starts again at 200x50... 3 sets of 200 for each ABR
+abrs_to_test_iter = rep(rep(abrs_to_test, each=iters_per_paramset), length(unique(kEs)))
 
 # no treatment to be given
 give_treat = 0
@@ -36,14 +37,15 @@ output <- ep.equi.sim(
   treat.start = treatment_start_year,
   treat.stop = treatment_stop_year,
   pnc = 0.05,
-  min.mont.age = 5,
+  min.mont.age = 0,
   vector.control.strt = NA,
   gam.dis.in = kE,
   run_equilibrium = TRUE,
   morbidity_module = "YES",
   equilibrium = NA,
   print_progress = TRUE,
-  prob_serorevert_fast = 0.5
+  prob_serorevert_fast = 0.5,
+  output_age_groups = list(c(0, 5), c(5, 10), c(10, 15), c(15, 20), c(20, 30), c(30, 40) c(40, 50), c(50, 60), c(60, 70), c(70, 81))
 )
 
 params <- list(ABR_in, kE)
